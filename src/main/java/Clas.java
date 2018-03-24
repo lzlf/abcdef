591cf7f377c256e942e056249b81d22182af5c8f
linzhanglong <15110014799@163.com>
2018-03-24 16:13:48 +0800

    Initial commit

---
+++ src/main/java/Clas.java
@@ -0,0 +1,12 @@
+/**
+ * @author linzhanglong
+ * @create 2018-03-24  15:58
+ * @desc 这是一个类
+ **/
+public class Clas {
+
+    public static void main(String[] args) {
+
+        System.out.println(324);
+    }
+}
